f13d14db7816d93d954286bd4b5c78769fa21414
aibarista <192158931+aibarista@noreply.github.com>
2025-02-07 15:14:03 -0500

    complete the backend to generate the intent

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,27 @@
 const express = require("express");
-const Stripe = require("stripe");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
-const stripe = Stripe("sk_test_51QkWrf2VX3dt2aloCSqPQg7IGY9011FPiPSlLZoY4bOqhgmjXMKuTz3kFNAHaK3CqTAGaFWWwK1pZqqMzELG3l4v00bqx2KTCP");
+require('dotenv').config();
+const stripe = require("stripe")(process.env.stripeSecretKey);
 
+app.use(express.static("./public"));
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
-app.post("/create-intent", async (req, res) => {
-  try {
-    const { paymentMethodId, amount } = req.body;
+app.post('/create-intent', async (req, res) => {
+    const { customerId, amount } = req.body;
 
-    // Validate request body
-    if (!paymentMethodId || !amount) {
-      return res.status(400).json({ error: "Missing required fields: paymentMethodId or amount." });
-    }
-
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-      payment_method: paymentMethodId,
-      confirmation_method: "manual", // Manual for PaymentRequest API
-      confirm: true,
+    const intent = await stripe.paymentIntents.create({
+        customer: process.env.stripeCustomerID,
+        amount: Number(amount),
+        currency: 'usd',
     });
-
-    res.json({ clientSecret: paymentIntent.client_secret });
-    console.log("send client Secret")
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+    return res.json({ client_secret: intent.client_secret });
 });
 
-const PORT = 3001;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
